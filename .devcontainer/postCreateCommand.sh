e66928b5eb6b125df218183b026e76545b3fc032
cameron turner <17177161+cturner8@users.noreply.github.com>
2023-07-20 18:18:42 +0000

    add awslocal aliases to shell config files on create

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,4 +2,7 @@
 
 sudo chown vscode /home/vscode/.cache/pip
 
+echo 'alias awslocal="AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test AWS_DEFAULT_REGION=${DEFAULT_REGION:-$AWS_DEFAULT_REGION} aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}"' >> /home/vscode/.bashrc
+echo 'alias awslocal="AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test AWS_DEFAULT_REGION=${DEFAULT_REGION:-$AWS_DEFAULT_REGION} aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}"' >> /home/vscode/.zshrc
+
 pip install -r app/requirements.txt
